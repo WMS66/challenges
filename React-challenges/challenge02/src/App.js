ee966541607fae4844b3e6c3c2c29a4355c69989
Wanderley Cabral <wanderleycabral@iMac-de-Wanderley.local>
2022-12-25 18:20:59 -0300

    Create Challenge #02

---
+++ React-challenges/challenge02/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Challenge #02</p>
+    </div>
+  );
+}
+
+export default App;
